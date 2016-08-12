181f8c5df6c9924d27fd44c907b88bd3826fe879
dag0bah1980 <clorico@gmail.com>
2016-08-11 23:52:31 -0400

    Issue re: Title
    After moving Title from AppComponent to Sidebarnav, the CLI crashed and
    wouldn't start the app. Removed call in app.component.spec.ts and now it
    works.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -11,10 +13,5 @@ describe('App: Angular2CRM', () => {
   it('should create the app',
     inject([AppComponent], (app: AppComponent) => {
       expect(app).toBeTruthy();
-    }));
-
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
     }));
 });
