6ab3276743c464e851946dac1a184b44da92f181
dag0bah1980 <clorico@gmail.com>
2016-08-12 00:28:59 -0400

    Upgrade from RC4 to RC5
    Implemented changes so that project is in RC5

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.Component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
