53c8875f92d52e7641471e9d4160bdf40c7f5f67
dag0bah1980 <clorico@gmail.com>
2017-04-02 08:49:03 -0400

    Trying to implement app version into cookie from package.json
    Tried to get version from package.json but it didn't work. I just built
    the section in the cookie window for now.

--- src/app/auth/modals/cookie-contents/cookie-contents.component.ts
+++ src/app/auth/modals/cookie-contents/cookie-contents.component.ts
@@ -11,16 +11,23 @@ import { CookieService } from 'angular2-cookie';
 })
 export class CookieContentsComponent implements OnInit {
 
-  constructor(private _cookieService:CookieService ) { }
+  constructor(private _cookieService:CookieService ) {
+
+   }
+
+  private appVersion;
 
   currentUserName: String;
   currentCookieTest: String;
   currentJWTToken: String;
+  currentAppVersion: String;
 
   ngOnInit() {
     this.currentUserName = this._cookieService.get('USER');
     this.currentCookieTest = this._cookieService.get('cookietest');
     this.currentJWTToken = this._cookieService.get('token');
+
+    this.currentAppVersion = this._cookieService.get('0.0.1');
   }
 
 }
---
+++ src/globals.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+export const version: string="0.0.1";    
