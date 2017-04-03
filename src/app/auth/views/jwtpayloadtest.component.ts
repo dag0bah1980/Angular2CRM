e96cfa3527b7f3d3947a719366fa6a9c04580011
dag0bah1980 <clorico@gmail.com>
2017-04-03 01:26:19 -0400

    Still playing around with JWT and Headers
    I have a better understanding now, but need to figure out why the use of
    the JWT is failing.

--- src/app/auth/views/jwtpayloadtest.component.ts
+++ src/app/auth/views/jwtpayloadtest.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 import { CookieService } from 'angular2-cookie';
 
+import { HttpService } from '../../auth/services/http.service';
+
+import { Http, Response, Request, RequestMethod, Headers } from '@angular/http';
+
 
 @Component({
   selector: 'ang2-crm-jwtpayloadtest',
   templateUrl: './jwtpayloadtest.component.html',
-  styleUrls: ['./jwtpayloadtest.component.css']
+  styleUrls: ['./jwtpayloadtest.component.css'],
+  providers: [HttpService]
 })
 export class JwtpayloadtestComponent implements OnInit {
 
-  constructor(private _cookieService:CookieService ) { }
+  constructor(private _cookieService:CookieService, public http: Http ) { }
 
-  currentUserName: String;
-  currentCookieTest: String;
-  currentJWTToken: String;
-  currentAppVersion: String;
-  currentHeaderPayloadSignature: String;
+  currentUserName: string;
+  currentCookieTest: string;
+  currentJWTToken: string;
+  currentAppVersion: string;
+  currentHeaderPayloadSignature: string;
 
   ngOnInit() {
     this.currentUserName = this._cookieService.get('USER');
@@ -25,7 +30,34 @@ export class JwtpayloadtestComponent implements OnInit {
     this.currentAppVersion = this._cookieService.get('appversion');
   }
 
-  createJWT() {
-    this.currentHeaderPayloadSignature = "Clicked!";
+   payload = {
+    foo: 'bar'
+  };
+
+
+
+  sendJWTAPICall() {
+      const body = JSON.stringify(this.payload);
+      let postheaders = new Headers();
+      
+      postheaders.append('Content-Type', 'application/json');
+      postheaders.append('Authorization', 'Basic ' + btoa(this.currentJWTToken));
+      return this.http.post('http://lorico.redirectme.net:8888/auth/foo', body, { headers: postheaders })
+      .map((data: Response) => data.json());
   }
+
+  tryJWTPost() {
+
+    //From Async undefined issues: http://stackoverflow.com/questions/41709346/angular-2-local-variable-is-undefined-outside-method    
+    this.sendJWTAPICall().subscribe(
+      data => { 
+          console.log('success');
+      },
+      error => { 
+        console.log('Error: ' + <any>error);
+        
+      },
+      () => {}
+    );   
+  } 
 }
