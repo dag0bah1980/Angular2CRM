5f3c6f8f3e11346282510ba63cd1c05ffa0ae1fe
dag0bah1980 <clorico@gmail.com>
2017-04-04 23:55:09 -0400

    Testing locking function / CSS toggling
    Adding locking functions based on clicks through CSS in scratch page.

--- src/app/auth/views/scratchpage.component.ts
+++ src/app/auth/views/scratchpage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'ang2-crm-scratchpage',
   templateUrl: './scratchpage.component.html',
@@ -7,15 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScratchpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this._fb.group({
+      'username' : [null, Validators.required],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(16)])]
+      })
   }
+
+  addOverlayDivBox = true;
 
   disableDiv(string){
     console.log(string);
-    this.addOverlay ? this.addOverlay = false : this.addOverlay = true;
+    this.addOverlayDivBox ? this.addOverlayDivBox = false : this.addOverlayDivBox = true;
   }
 
-  addOverlay = true;
+  loginForm: FormGroup;
+  toggleSubmit=false;
+  lockLogin = false;
+  submitFormClean(value: any) {
+      console.log('SubmitFormClean clicked!' + value);
+      this.toggleSubmit=true;
+      this.lockLogin ? this.lockLogin = false : this.lockLogin = true;
+  }
 }
