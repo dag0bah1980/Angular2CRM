8efe66e59d8f7e21cf5f3bb7f12f303d48ae399b
dag0bah1980 <clorico@gmail.com>
2017-05-16 10:09:22 -0400

    Created a few different time / date types for that pipe
    Created a few different time / date types for that pipe. Also added
    moment-timezone.js

--- src/app/auth/pipes/timedate.pipe.ts
+++ src/app/auth/pipes/timedate.pipe.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
+
+
 
 @Pipe({
   name: 'timedate'
@@ -9,12 +11,21 @@ export class TimedatePipe implements PipeTransform {
 
   transform(date: any, arg1: any): any {
     //this.value = this.value | 'EEE MMM d, h:mm:ss a';
-    let d = new Date(date)
-    if (arg1 == 'noyear' ){
-      return moment(d).format('ddd MMM DD[@]h:mm:ss a');  
+    let d = new Date(date)    
+    if (arg1 == 'short' ){
+      return moment(d).format('MMM YYYY[@]h:mma');  
+    } 
+    else if (arg1 == 'medium') {
+      return moment(d).format('MMM DD YYYY[@]h:mm a');  
+    }
+    else if (arg1 == 'long') {
+      return moment(d).format('ddd MMM DD YYYY[@]h:mm:ss a');
+    } 
+    else if (arg1 == 'extralong') {
+      return moment(d).tz('America/Toronto').format('ddd MMM DD YYYY[@]h:mm:ss a zz');
     }
     else {
-      return moment(d).format('ddd MMM DD YYYY[@]h:mm:ss a');
+      return moment(d).format('MMM YYYY[@]h:mm:ss a'); 
     }
   }
 
