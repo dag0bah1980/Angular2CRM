0f96c6b93a84ea1cbacabfc1b4163fbb64509a78
dag0bah1980 <clorico@gmail.com>
2017-07-17 23:53:05 -0400

    Fixed Side Bar Navigation for Tags and added some commenting
    Fixed Side Bar Navigation for Tags and added some commenting for
    clarity. Cleaned up the code to remove things that were not needed.

--- src/app/auth/views/createtag.component.ts
+++ src/app/auth/views/createtag.component.ts
@@ -1,48 +1,58 @@
+// Standard Angular Stuff...
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 import { FormGroup, FormBuilder, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
 
-import { HttpService } from '../services/http.service';
-import { TagsService } from '../services/data/tags.service';
+//import { HttpService } from '../services/http.service';
+
+// Service for formatting date / time
 import { TimedatePipe } from '../pipes/timedate.pipe';
 
+// Service for getting details of Cookies.
 import { CookieService } from 'angular2-cookie';
 
+// Third party imports
 import { Subscription, Observable } from 'rxjs/Rx';
 import 'rxjs/add/observable/of';
-import { TypeaheadMatch } from 'ngx-bootstrap/typeahead';
 import { SelectItem } from 'primeng/primeng';
 
-
+// Class and Data Service for Class
 import { Tag } from '../class/tag';
-
+import { TagsService } from '../services/data/tags.service';
 
 
 @Component({
   selector: 'ang2-crm-createtag',
   templateUrl: './createtag.component.html',
   styleUrls: ['./createtag.component.css'],
-  providers: [HttpService, TagsService]
+  providers: [TagsService]
 })
+
+
 export class CreatetagComponent implements OnInit {
 
   private subscription: Subscription;
   id: number;
-  activeTag: Tag;  
+  activeTag: Tag;
 
+  // Fields that are mapped to elements of the class: Tag
   IsActive: boolean;
   IsDeleted: boolean;
   Tag: string;
   Description: string;
   Created: string;
   Modified: string;
-  
-  dataForm: FormGroup;
-  createdTag: Tag; 
 
+  // Name of the form I'm using to create a tag
+  dataForm: FormGroup;
+
+  // Tag instance used when creating a Tag
+  createdTag: Tag;
+
+  // ???
   private data;
+  
   //errors is for form validation errors
   private errors;
 
@@ -63,12 +73,12 @@ export class CreatetagComponent implements OnInit {
   // The following are for dropdowns:
   //cities: SelectItem[];
   //exampleSelectedCity: string;
-  
-  
 
-  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, 
-  private _tagService: TagsService, private _cookieService: CookieService, 
-  private _router: Router, private _fb: FormBuilder) {
+
+  // Constructor.
+  constructor(private activatedRoute: ActivatedRoute,
+    private _tagService: TagsService, private _cookieService: CookieService,
+    private _router: Router, private _fb: FormBuilder) {
     this.dataForm = this._fb.group({
       IsActive: new FormControl(),
       IsDeleted: new FormControl(),
@@ -90,16 +100,19 @@ export class CreatetagComponent implements OnInit {
     this.cities.push({label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}});
     */
 
-   }
+  }
 
   ngOnInit() {
+
+    // Code Validation Regular Express (Only 32 characters, with only letters (lower / upper) and numbers allowed)
     let CodeValidationRegex = '[a-zA-Z0-9]{0,32}';
 
+    // Initialize Form Group with default settings. Example Fields have been commented out.
     this.dataForm = this._fb.group({
-      'IsActive' : [true, Validators.required],
-      'IsDeleted' : [false, Validators.required],
+      'IsActive': [true, Validators.required],
+      'IsDeleted': [false, Validators.required],
       'Tag': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(32), Validators.pattern(CodeValidationRegex)])],
-      'Description' : [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(512)])],      
+      'Description': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(512)])],
       /* 'exampleDate' : [null, Validators.required] */
       /* 'exampleWYSIWYG': ['', Validators.required], */
       /* 'exampleSelectedCity': [null,Validators.required] */
@@ -108,21 +121,21 @@ export class CreatetagComponent implements OnInit {
     this.active = true;
     this.statusMessage = null;
     this.statusMessageEmpty = true;
-    
+
 
     //testing for form changes.
-    
+
     this.formCodeSub = this.dataForm.valueChanges.subscribe(data => {
       //console.log('Form changes', data)
       //console.log('Trying to get Tag value', data.Tag)
       if (this.statusMessageEmpty = true) {
-        if(data.Tag!=this.TagValue) {
-          this.statusMessage=null;
-          this.statusMessageEmpty=true;        
+        if (data.Tag != this.TagValue) {
+          this.statusMessage = null;
+          this.statusMessageEmpty = true;
         }
       }
       this.output = data;
-    });    
+    });
   }
 
   private TagLength: number = 0;
@@ -130,8 +143,8 @@ export class CreatetagComponent implements OnInit {
     var length;
     if (this.dataForm.controls[formControlField] === null) {
       return 0;
-    } 
-    if (!this.dataForm.controls[formControlField].pristine){
+    }
+    if (!this.dataForm.controls[formControlField].pristine) {
       length = this.dataForm.controls[formControlField].value.length;
       return length;
     }
@@ -142,32 +155,31 @@ export class CreatetagComponent implements OnInit {
     var value;
     if (this.dataForm.controls[formControlField] === null) {
       return '';
-    } 
-    if (!this.dataForm.controls[formControlField].pristine){
+    }
+    if (!this.dataForm.controls[formControlField].pristine) {
       value = this.dataForm.controls[formControlField].value;
       return value;
     }
   }
 
-  checkValidAndPristine(controlValue: string){
+  checkValidAndPristine(controlValue: string) {
     if (!this.dataForm.controls[controlValue].valid && !this.dataForm.controls[controlValue].pristine) {
       return true;
-    } else
-    {
+    } else {
       return false;
     }
   }
- 
-  resetForm(){
+
+  resetForm() {
     this.dataForm.reset();
     //console.log('Tag:' + JSON.stringify(this.updatedTag));
 
     let CodeValidationRegex = '[a-zA-Z0-9]{0,32}';
     this.dataForm = this._fb.group({
-      'IsActive' : [true, Validators.required],
-      'IsDeleted' : [false, Validators.required],
+      'IsActive': [true, Validators.required],
+      'IsDeleted': [false, Validators.required],
       'Tag': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(32), Validators.pattern(CodeValidationRegex)])],
-      'Description' : [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(16)])],
+      'Description': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(16)])],
       /* 'exampleDate' : [null, Validators.required]*/
       /* 'exampleWYSIWYG': [null, Validators.required]*/
       /* 'exampleSelectedCity': [null,Validators.required] */
@@ -185,102 +197,102 @@ export class CreatetagComponent implements OnInit {
     this.dataForm.controls['Description'].setValue(null);
     this.dataForm.controls['Description'].markAsPristine;
 
-    this.dataForm
+    //this.dataForm
   }
 
-  onCancelClick(){
+  onCancelClick() {
     //console.log('Clicked Cancel!');
-    this.resetForm();  
+    this.resetForm();
   }
 
   navBackToList() {
     this._router.navigateByUrl('/auth/listtags');
   }
- 
-    onSubmit() {
-      
-      console.log('Clicked Submit');
-      this.createdTag = new Tag(0, '', '', false, false, '','');
 
-      this.createdTag.ISACTIVE = this.dataForm.controls['IsActive'].value;
-      this.createdTag.ISDELETED = this.dataForm.controls['IsDeleted'].value;
-      this.createdTag.TAG = this.dataForm.controls['Tag'].value;
-      this.createdTag.DESCRIPTION = this.dataForm.controls['Description'].value;
+  onSubmit() {
 
-      var TagValue;
-      this.TagValue = this.createdTag.TAG;
+    console.log('Clicked Submit');
+    this.createdTag = new Tag(0, '', '', false, false, '', '');
 
-      
-      //console.log(this.createdTag);
-      console.log(this.statusMessage + ':' + this.statusMessageEmpty);
-      this._tagService.createTag(this.createdTag)
-        .subscribe(result => {          
-          if (result == true) {                       
-            this.statusMessage = 'SUCCESS';
-            this.statusMessageEmpty = false;            
-          }
-          else {
-            this.statusMessage = 'FAIL';
-            this.statusMessageEmpty = false;                      
-          }
-        });
+    this.createdTag.ISACTIVE = this.dataForm.controls['IsActive'].value;
+    this.createdTag.ISDELETED = this.dataForm.controls['IsDeleted'].value;
+    this.createdTag.TAG = this.dataForm.controls['Tag'].value;
+    this.createdTag.DESCRIPTION = this.dataForm.controls['Description'].value;
+
+    var TagValue;
+    this.TagValue = this.createdTag.TAG;
+
+
+    //console.log(this.createdTag);
+    console.log(this.statusMessage + ':' + this.statusMessageEmpty);
+    this._tagService.createTag(this.createdTag)
+      .subscribe(result => {
+        if (result == true) {
+          this.statusMessage = 'SUCCESS';
+          this.statusMessageEmpty = false;
+        }
+        else {
+          this.statusMessage = 'FAIL';
+          this.statusMessageEmpty = false;
+        }
+      });
+  }
+
+  private source;
+  private length;
+  private editortext;
+
+  private DescriptionLength = 0;
+
+
+  private onTextChange($event): void {
+    //refer to this page to add class on class ui-editor-container:
+    //https://stackoverflow.com/questions/38944725/how-to-get-dom-element-in-angular-2
+    //this.source = $event.source;
+    //this.length = $event.textValue.length;
+    //this.editortext = $event.textValue;
+
+    let myExampleEditor = document.querySelector(".ui-editor-container");
+
+    if ($event.textValue.length > 1) {
+      //console.log('has some text');
+      myExampleEditor.classList.remove("ng-invalid");
+      this.DescriptionLength = $event.htmlValue.length;
+    } else {
+      //console.log('NO TEXT');
+      this.DescriptionLength = 0;
+      myExampleEditor.classList.add("ng-invalid");
     }
 
-    public source;
-    public length;
-    public editortext;
+  }
 
-    private DescriptionLength = 0;
+  private onInit($event): void {
+    //refer to this page to add class on class ui-editor-container:
+    //https://stackoverflow.com/questions/38944725/how-to-get-dom-element-in-angular-2
+    //this.source = $event.source;
+    //this.length = $event.textValue.length;
+    //this.editortext = $event.textValue;
 
+    let myExampleEditor = document.querySelector(".ui-editor-container");
 
-    public onTextChange($event): void {
-      //refer to this page to add class on class ui-editor-container:
-      //https://stackoverflow.com/questions/38944725/how-to-get-dom-element-in-angular-2
-      //this.source = $event.source;
-      //this.length = $event.textValue.length;
-      //this.editortext = $event.textValue;
-      
-       let myExampleEditor = document.querySelector(".ui-editor-container");
-
-      if($event.textValue.length>1){
-        //console.log('has some text');
-        myExampleEditor.classList.remove("ng-invalid");
-        this.DescriptionLength = $event.htmlValue.length;
-      } else {
-        //console.log('NO TEXT');
-        this.DescriptionLength = 0;
-        myExampleEditor.classList.add("ng-invalid");
-      }
-      
+    if ($event.textValue == null) {
+      myExampleEditor.classList.add("ng-invalid");
+      this.DescriptionLength = 0;
+    } else {
+      myExampleEditor.classList.remove("ng-invalid");
+      this.DescriptionLength = $event.htmlValue.length;
     }
 
-    public onInit($event): void {
-      //refer to this page to add class on class ui-editor-container:
-      //https://stackoverflow.com/questions/38944725/how-to-get-dom-element-in-angular-2
-      //this.source = $event.source;
-      //this.length = $event.textValue.length;
-      //this.editortext = $event.textValue;
-      
-      let myExampleEditor = document.querySelector(".ui-editor-container");
+  }
 
-      if($event.textValue==null){
-        myExampleEditor.classList.add("ng-invalid");
-        this.DescriptionLength =0;
-      } else {
-        myExampleEditor.classList.remove("ng-invalid");
-        this.DescriptionLength = $event.htmlValue.length;
-      }
-      
-    }
 
-    
-    public onChange($event): void {
-      let myExampleDropDown = document.querySelector(".ui-dropdown");
-      
-      if($event.value==null){
-        myExampleDropDown.classList.add("ng-invalid");
-      } else {
-        myExampleDropDown.classList.remove("ng-invalid");
-      }
+  private onChange($event): void {
+    let myExampleDropDown = document.querySelector(".ui-dropdown");
+
+    if ($event.value == null) {
+      myExampleDropDown.classList.add("ng-invalid");
+    } else {
+      myExampleDropDown.classList.remove("ng-invalid");
     }
+  }
 }
