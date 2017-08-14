0eb1f3690ee09ab9d8262f35a752c5378f80257e
dag0bah1980 <clorico@gmail.com>
2017-08-14 00:52:57 -0400

    Playing with a heatmap calendar under the style guides view
    I want to implement a heatmap calendar, trying to build one with
    Angular2 through this Python guide:
    https://github.com/alexwlchan/contributions-graph

--- src/app/auth/views/styleguide.component.ts
+++ src/app/auth/views/styleguide.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StyleguideComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
