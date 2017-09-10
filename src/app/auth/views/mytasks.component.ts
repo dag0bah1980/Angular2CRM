735767c9fd629599f82268356a7511a355ef105d
dag0bah1980 <clorico@gmail.com>
2017-09-10 07:14:14 -0400

    Got expandable and selectable functions working in mytasks grid
    Added selectable and expandable functions, which I want to try with
    projects list.

--- src/app/auth/views/mytasks.component.ts
+++ src/app/auth/views/mytasks.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Car } from '../class/Car';
 
 import { CarService } from '../services/data/cars.service';
+import {SelectItem} from 'primeng/primeng';
+import {Message} from  'primeng/primeng';
 
 @Component({
   selector: 'ang2-crm-mytasks',
@@ -15,10 +17,44 @@ export class MytasksComponent implements OnInit {
 
  cars: Car[];
 
+  brands: SelectItem[];
+
+  msgs: Message[] = [];
+  
+   cols: any[];
+    
+    selectedCar: Car;
+    
+    dialogVisible: boolean;
   constructor(private _carService: CarService) { }
 
   ngOnInit() {
     this._carService.getCarsSmall().then(cars => this.cars = cars);
+
+    this.brands = [
+            {label: 'Choose', value: null},
+            {label: 'Audi', value: 'Audi'},
+            {label: 'BMW', value: 'BMW'},
+            {label: 'Fiat', value: 'Fiat'},
+            {label: 'Ford', value: 'Ford'},
+            {label: 'Honda', value: 'Honda'},
+            {label: 'Jaguar', value: 'Jaguar'},
+            {label: 'Mercedes', value: 'Mercedes'},
+            {label: 'Renault', value: 'Renault'},
+            {label: 'VW', value: 'VW'},
+            {label: 'Volvo', value: 'Volvo'}
+        ];
   }
 
+  selectCar(car: Car) {
+        this.msgs = [];
+        this.msgs.push({severity:'info', summary:'Car Select', detail:'Vin: ' + car.vin});
+    }
+
+      showCar(car: Car) {
+        this.selectedCar = car;
+        this.dialogVisible = true;
+    }
+
+  
 }
